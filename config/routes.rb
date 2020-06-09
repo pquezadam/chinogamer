5ae3f5653753553407e3f340e46470a172d817e6
pquezadam <pquezada2009@hotmail.es>
2020-06-09 19:39:34 -0400

    orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'orders/create'
   resources :products, only: :index do
     resources :orders, only: :create
   end
-  root to: 'products#index'
 
+  resources :orders, only: :index
+
+  root to: 'products#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
